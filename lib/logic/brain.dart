e1f67ab0eabf008104bc19fe26cabab67bff6272
Emmanuel <ogasule001@gmail.com>
2020-09-22 15:37:55 +0100

    remove variable not in use

--- lib/logic/brain.dart
+++ lib/logic/brain.dart
@@ -1,13 +1,12 @@
 class TicTacToe {
   int movesPlayed = 0;
   List<String> board = ["", "", "", "", "", "", "", "", ""];
-  bool gameEnded = false;
   List<String> letters = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
   bool win = false;
   void clearBoard() {
     board = ["", "", "", "", "", "", "", "", ""];
     movesPlayed = 0;
-    gameEnded = false;
+    win = false;
   }
 
   bool checkBoard(int position) {
@@ -33,62 +32,28 @@ class TicTacToe {
       if (board[0].isNotEmpty) {
         if ((board[0] == board[1]) && (board[1] == board[2])) {
           win = true;
-          //gameEnded = true;
         } else if ((board[0] == board[3]) && (board[3] == board[6])) {
           win = true;
-          //gameEnded = true;
         } else if ((board[0] == board[4]) && (board[4] == board[8])) {
           win = true;
-          //gameEnded = true;
         }
       }
       if (board[8].isNotEmpty) {
         if ((board[6] == board[7]) && (board[7] == board[8])) {
           win = true;
-          //gameEnded = true;
         } else if ((board[2] == board[5]) && (board[5] == board[8])) {
           win = true;
-          //gameEnded = true;
         }
       }
       if (board[4].isNotEmpty) {
         if ((board[1] == board[4]) && (board[4] == board[7])) {
           win = true;
-          //gameEnded = true;
         } else if ((board[3] == board[4]) && (board[4] == board[5])) {
           win = true;
-          //gameEnded = true;
         } else if ((board[2] == board[4]) && (board[4] == board[6])) {
           win = true;
-          //gameEnded = true;
         }
       }
     }
   }
 }
-
-// if ((board[0] == board[1]) && (board[1] == board[2])) {
-//   win = true;
-//   //gameEnded = true;
-// } else if ((board[0] == board[3]) && (board[3] == board[6])) {
-//   win = true;
-//   //gameEnded = true;
-// } else if ((board[0] == board[4]) && (board[4] == board[8])) {
-//   win = true;
-//   //gameEnded = true;
-// } else if ((board[3] == board[4]) && (board[4] == board[5])) {
-//   win = true;
-//   //gameEnded = true;
-// } else if ((board[6] == board[7]) && (board[7] == board[8])) {
-//   win = true;
-//   //gameEnded = true;
-// } else if ((board[2] == board[5]) && (board[5] == board[8])) {
-//   win = true;
-//   //gameEnded = true;
-// } else if ((board[1] == board[4]) && (board[4] == board[7])) {
-//   win = true;
-//   //gameEnded = true;
-// } else if ((board[2] == board[4]) && (board[4] == board[6])) {
-//   win = true;
-//   //gameEnded = true;
-// }
