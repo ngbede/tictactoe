69bbd5be0d00770745222cc723dc5e2605b25746
Emmanuel <ogasule001@gmail.com>
2020-10-15 11:56:58 +0100

    making TicTacToe class  variables private

--- lib/logic/brain.dart
+++ lib/logic/brain.dart
@@ -1,68 +1,68 @@
 import 'package:flutter/foundation.dart';
 
 class TicTacToe with ChangeNotifier {
-  int movesPlayed = 0;
-  List<String> board = ["", "", "", "", "", "", "", "", ""];
-  List<String> letters = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-  bool win = false;
+  int _movesPlayed = 0;
+  List<String> _board = ["", "", "", "", "", "", "", "", ""];
+  List<String> _letters = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+  bool _win = false;
   void clearBoard() {
-    board = ["", "", "", "", "", "", "", "", ""];
-    movesPlayed = 0;
-    win = false;
+    _board = ["", "", "", "", "", "", "", "", ""];
+    _movesPlayed = 0;
+    _win = false;
     notifyListeners();
   }
 
   void checkBoard(int position) {
-    if ((board[position].isEmpty) && (win != true)) {
-      movesPlayed += 1;
-      board[position] = letters[movesPlayed - 1];
+    if ((_board[position].isEmpty) && (_win != true)) {
+      _movesPlayed += 1;
+      _board[position] = _letters[_movesPlayed - 1];
       notifyListeners();
     }
   }
 
   String letterAtPosition(int pos) {
-    return board[pos];
+    return _board[pos];
   }
 
   String message() {
     String letter;
-    if (movesPlayed > 8 && win != true) {
+    if (_movesPlayed > 8 && _win != true) {
       letter = 'Game ended in a draw!';
-    } else if (win == true) {
-      letter = '${letters[movesPlayed - 1]} has won the game';
+    } else if (_win == true) {
+      letter = '${_letters[_movesPlayed - 1]} has won the game';
     } else {
-      if (win != true) {
-        letter = '${letters[movesPlayed]} turn to play';
+      if (_win != true) {
+        letter = '${_letters[_movesPlayed]} turn to play';
       }
     }
     return letter;
   }
 
   void analyseBoard() {
-    if ((movesPlayed > 4) && (win == false)) {
-      if (board[0].isNotEmpty) {
-        if ((board[0] == board[1]) && (board[1] == board[2])) {
-          win = true;
-        } else if ((board[0] == board[3]) && (board[3] == board[6])) {
-          win = true;
-        } else if ((board[0] == board[4]) && (board[4] == board[8])) {
-          win = true;
+    if ((_movesPlayed > 4) && (_win == false)) {
+      if (_board[0].isNotEmpty) {
+        if ((_board[0] == _board[1]) && (_board[1] == _board[2])) {
+          _win = true;
+        } else if ((_board[0] == _board[3]) && (_board[3] == _board[6])) {
+          _win = true;
+        } else if ((_board[0] == _board[4]) && (_board[4] == _board[8])) {
+          _win = true;
         }
       }
-      if (board[8].isNotEmpty) {
-        if ((board[6] == board[7]) && (board[7] == board[8])) {
-          win = true;
-        } else if ((board[2] == board[5]) && (board[5] == board[8])) {
-          win = true;
+      if (_board[8].isNotEmpty) {
+        if ((_board[6] == _board[7]) && (_board[7] == _board[8])) {
+          _win = true;
+        } else if ((_board[2] == _board[5]) && (_board[5] == _board[8])) {
+          _win = true;
         }
       }
-      if (board[4].isNotEmpty) {
-        if ((board[1] == board[4]) && (board[4] == board[7])) {
-          win = true;
-        } else if ((board[3] == board[4]) && (board[4] == board[5])) {
-          win = true;
-        } else if ((board[2] == board[4]) && (board[4] == board[6])) {
-          win = true;
+      if (_board[4].isNotEmpty) {
+        if ((_board[1] == _board[4]) && (_board[4] == _board[7])) {
+          _win = true;
+        } else if ((_board[3] == _board[4]) && (_board[4] == _board[5])) {
+          _win = true;
+        } else if ((_board[2] == _board[4]) && (_board[4] == _board[6])) {
+          _win = true;
         }
       }
     }
